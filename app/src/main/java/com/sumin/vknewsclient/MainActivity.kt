a80900c4c2c7fa3876ffd1e38ff23139b8499220
amnt <skillovoypresent@gmail.com>
2025-09-30 13:01:06 +0300

    lesson1

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -3,13 +3,18 @@ package com.sumin.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sumin.vknewsclient.ui.theme.PostCard
 import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +23,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             VkNewsClientTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
                 ) {
-                    Greeting("Android")
+                    PostCard()
                 }
             }
         }
--- app/src/main/java/com/sumin/vknewsclient/ui/theme/Theme.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/theme/Theme.kt
@@ -1,22 +1,26 @@
 package com.sumin.vknewsclient.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = darkColorScheme(
+    primary = Black900,
+    primaryContainer = Black900,
+    secondary = Black900,
+    onPrimary = Color.White,
+    onSecondary = Black500
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
+private val LightColorPalette = lightColorScheme(
+    primary = Color.White,
+    primaryContainer = Color.White,
+    secondary = Color.White,
+    onPrimary = Black900,
+    onSecondary = Black500
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
@@ -36,7 +40,7 @@ fun VkNewsClientTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Comp
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
